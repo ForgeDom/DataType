4c560e2fd30185282a38a17f80af7c6babf96d6e
ForgeDom <maxstill2006@gmail.com>
2024-04-20 17:28:22 +0300

    Ex 1 from hometask is done

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 #include <Windows.h>
 using namespace std;
 
+double formula(double r1, double r2, double r3) {
+	return 1/(1 / r1 + 1 / r2 + 1 / r3);
+}
+
 int main() {
-	for (size_t i = 0; i <= 255; i++) {
-		cout << i << " - " << char(i) << endl;
-	}
+	int r1, r2, r3;
+	cin >> r1 >> r2 >> r3;
+	double r0 = formula(r1, r2, r3);
+	cout << r0 << endl;
 	return 0;
 }
