a939c314140ac355c289d637cfdfb4456109b62f
ForgeDom <maxstill2006@gmail.com>
2024-04-20 16:28:56 +0300

    Ex 1 from lab is done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,23 @@
 using namespace std;
 
 int main() {
-	for (size_t i = 0; i <= 255; i++) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	/*for (size_t i = 0; i <= 255; i++) {
 		cout << i << " - " << char(i) << endl;
-	}
+	}*/
+	char str1[] = "The war and the peace";
+	char str2[] = "L.N.Tolstoj";
+	char str3[] = "Piter";
+	int x = 500;
+	cout << "\tName       : ";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << str1 << endl;
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "\tAvtor      :   " << str2 << endl;
+	cout << "\tIzdatelstvo:  " << str3 << endl;
+	cout << "\tPages      : ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cout << x << endl;
+	SetConsoleTextAttribute(hConsole, 0x0F);
 	return 0;
 }
