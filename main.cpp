8a2f174f32aa5bcbe5c4dcbdcfc0342852176bb4
ForgeDom <maxstill2006@gmail.com>
2024-04-20 16:46:08 +0300

    Ex 2 from lab is done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@
 using namespace std;
 
 int main() {
-	for (size_t i = 0; i <= 255; i++) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	/*for (size_t i = 0; i <= 255; i++) {
 		cout << i << " - " << char(i) << endl;
-	}
+	}*/
+	string line = " U lukomor'a dub zelenij,\n Zlataya zep na dupe tom,\n I dnem i nochju kot uchenij\n vse hodit po cepi krugom";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << line;
+	SetConsoleTextAttribute(hConsole, 0x0F);
 	return 0;
 }
